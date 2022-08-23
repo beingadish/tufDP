3682e497eebd52fba7ddbe03dd8e7d5087b71c87
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-08-23 12:21:17 +0530

    dp_3.cpp File Updations

--- dp_3.cpp
+++ dp_3.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 // FROG JUMP (CodeStudio // CodingNinjas)
 
-int main(){
+int frogJump(int ind, vector<int> &energy){
 
+    if(ind)
+
+    int left = frogJump(ind+1,energy);
+    int right = frogJump(ind+2,energy);
+
+    return min(left,right) + abs(energy[ind+1]-)
+    
+}
+
+
+void inputArray(int n,vector<int> &arr){
+    for(int i =0;i<n;i++) cin >> arr[i];
+}
+
+int main(){
+    int stairs;
+    cout << "Enter the size of the staircase : ";
+    cin >> stairs;
+    vector<int> energy(stairs);
+    inputArray(stairs,energy);
+    cout << "Minimum Energy Required : ";
+    cout << solution(energy) << endl;
     return 0;
 }
