402c481702eb681444ac257ea60e0a301a7b2532
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-08-24 15:56:50 +0530

    DP_3

--- dp_3.cpp
+++ dp_3.cpp
@@ -3,19 +3,30 @@ using namespace std;
 
 // FROG JUMP (CodeStudio // CodingNinjas)
 
-int frogJump(int ind, vector<int> &energy){
+int frogJump(int ind, vector<int> &energy, vector<int> &dp){
 
-    if(ind)
+    if(ind == energy.size()-1) return 0;
 
-    int left = frogJump(ind+1,energy);
-    int right = frogJump(ind+2,energy);
+    int left = INT_MAX;
+    int right = INT_MAX;
 
-    return min(left,right) + abs(energy[ind+1]-)
+    if(dp[ind] != -1) return dp[ind];
+
+    else{
+        left = frogJump(ind+1,energy,dp) + abs(energy[ind] - energy[ind+1]);
+    if( ind < energy.size() - 2){
+        right = frogJump(ind+2,energy,dp) + abs(energy[ind] - energy[ind+2]);
+    }
+
+    return dp[ind] = min(left,right);
+
+    }
     
 }
 
 
 void inputArray(int n,vector<int> &arr){
+    cout << "Enter the Energy Level at each Staircase : ";
     for(int i =0;i<n;i++) cin >> arr[i];
 }
 
@@ -23,9 +34,9 @@ int main(){
     int stairs;
     cout << "Enter the size of the staircase : ";
     cin >> stairs;
-    vector<int> energy(stairs);
+    vector<int> energy(stairs),dp(stairs,-1);
     inputArray(stairs,energy);
     cout << "Minimum Energy Required : ";
-    cout << solution(energy) << endl;
+    cout << frogJump(0,energy,dp) << endl;
     return 0;
 }
