24835de88e2454dd0a41990bcbb4916cce096497
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-08-26 01:17:14 +0530

    DP 7 NINJA DP MEMOIZATION ADDED

--- dp_7.cpp
+++ dp_7.cpp
@@ -26,6 +26,29 @@ int ninja(int day, int last, vector<vector<int>> &pts){
     return maxpt;
 }
 
+// DP Implemented Solution (Memoization)
+
+int ninjaDP(int day, int last, vector<vector<int>> &pts, vector<vector<int>> &dp){
+    if(day == 0){
+        int maxpt =0;
+        for(int i =0;i<3;i++){
+            if(i != last) maxpt = max(maxpt,pts[0][i]);
+        }
+        return maxpt;
+    }
+    if(dp[day][last] != -1) return dp[day][last];
+    int maxpt = 0;
+    for(int j =0;j<3;j++){
+        if(j != last) {
+            int points = pts[day][j] + ninjaDP(day-1,j,pts,dp);
+            maxpt = max(maxpt,points);
+        }
+        
+    }
+    
+    return dp[day][last] = maxpt;
+}
+
 void input2dArray(vector<vector<int>> &arr){
     int j = arr.size();
     for(int i = 0;i<j;i++){
@@ -38,9 +61,8 @@ int main(){
     int n;
     cout << "Enter number of Days : ";
     cin >> n;
-    vector<int> temp(3,0);
-    vector<vector<int>> days(n,temp);
+    vector<vector<int>> days(n,vector<int>(3)),dp(n,vector<int>(4,-1));
     input2dArray(days);
-    cout << "The maximum points that the Ninja can Score by performing different tasks on adjacent days is : " << ninja(n-1,3,days) << endl << endl;
+    cout << "The maximum points that the Ninja can Score by performing different tasks on adjacent days is : " << ninjaDP(n-1,3,days,dp) << endl << endl;
     return 0;
 }
