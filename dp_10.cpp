8f448bdc31c71f2cd09c2e273cd12d4c8490013f
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-08-28 00:04:44 +0530

    DP 10 Soltion with MEMOIZATION & TABULATION

--- dp_10.cpp
+++ dp_10.cpp
@@ -31,6 +31,18 @@ void minPathSum(int m, int n, vector<vector<int>> &grid, int sum,vector<int> &an
 }
 
 
+// STRIVER's SOLUTION using Recursion (with Memoization)
+
+int minSumPath(int i, int j,vector<vector<int>> &grid,vector<vector<int>> &dp){
+    if(i == 0 && j == 0) return grid[i][j];
+    if(i < 0 || j < 0) return 1e9;
+    if(dp[i][j] != -1) return dp[i][j];
+    int up = grid[i][j] + minSumPath(i-1,j,grid,dp);
+    int left = grid[i][j] + minSumPath(i,j-1,grid,dp);
+    return dp[i][j] = min(up,left);
+}
+
+
 
 void input2dArray(vector<vector<int>> &arr){
     int j = arr.size();
@@ -41,12 +53,49 @@ void input2dArray(vector<vector<int>> &arr){
 }
 
 int solution(vector<vector<int>> &grid){
+
     int m = grid.size();
     int n = grid[0].size();
-    vector<int> ans;
-    minPathSum(m-1,n-1,grid,grid[m-1][n-1],ans);
-    sort(ans.begin(),ans.end());
-    return ans[0];
+    
+
+    // ACCORDING TO MY SOLUTION :
+
+    // vector<int> ans;
+    // minPathSum(m-1,n-1,grid,grid[m-1][n-1],ans);
+    // sort(ans.begin(),ans.end());
+    // return ans[0];
+
+
+
+    // ACCORDING TO STRIVER's SOLUTION
+
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    return minSumPath(grid.size()-1,grid[0].size()-1,grid,dp);
+
+}
+
+
+int tabulation(vector<vector<int>> &grid,vector<vector<int>> &dp){
+    
+    int m = grid.size();
+    int n = grid[0].size();
+    
+    for(int i =0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+            else{
+                int up = grid[i][j],left = grid[i][j];
+                if(i > 0) up+= dp[i-1][j];
+                else up += 1e9;
+                if(j > 0) left+= dp[i][j-1];
+                else left += 1e9;
+                dp[i][j] = min(up,left);
+            }
+        }
+    }
+    
+    return dp[m-1][n-1];
+    
 }
 
 
@@ -57,7 +106,7 @@ int main(){
     vector<vector<int>> grid(m,vector<int>(n));
     cout << "Enter the GRID COST Matrix : " << endl;
     input2dArray(grid);
-    // vector<vector<int>> dp(m,vector<int>(n,-1));
-    cout << "Minimum Path Sum from [0][0] to [" << m-1 << "][" << n-1 << "] is : " << solution(grid) << endl << endl;
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    cout << "Minimum Path Sum from [0][0] to [" << m-1 << "][" << n-1 << "] is : " << tabulation(grid,dp) << endl << endl;
     return 0;
 }
