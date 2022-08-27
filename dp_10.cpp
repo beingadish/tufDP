e02b9e04df4cfe07e06488a172ebd8f98d14f3b9
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-08-28 00:23:56 +0530

    SPACE OPTIMIZED CODE

--- dp_10.cpp
+++ dp_10.cpp
@@ -98,6 +98,30 @@ int tabulation(vector<vector<int>> &grid,vector<vector<int>> &dp){
     
 }
 
+// SPACE OPTIMISED CODE
+
+int spaceOPTI(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<int> prev(n,0);
+    for(int i =0;i<m;i++){
+        vector<int> cur(n,0);
+        for(int j=0;j<n;j++){
+            if(i == 0 && j == 0) cur[j] = grid[i][j];
+            else{
+                int up = grid[i][j],left = grid[i][j];
+                if(i > 0) up+= prev[j];
+                else up += 1e9;
+                if(j > 0) left+= cur[j-1];
+                else left += 1e9;
+                cur[j] = min(up,left);
+            }
+        }
+        prev = cur;
+    }
+    return prev[n-1];
+}
+
 
 int main(){
     int m , n;
@@ -107,6 +131,6 @@ int main(){
     cout << "Enter the GRID COST Matrix : " << endl;
     input2dArray(grid);
     vector<vector<int>> dp(m,vector<int>(n,-1));
-    cout << "Minimum Path Sum from [0][0] to [" << m-1 << "][" << n-1 << "] is : " << tabulation(grid,dp) << endl << endl;
+    cout << "Minimum Path Sum from [0][0] to [" << m-1 << "][" << n-1 << "] is : " << spaceOPTI(grid) << endl << endl;
     return 0;
 }
